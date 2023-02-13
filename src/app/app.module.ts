c527d97a543662c079966bc808040074e8d647c7
Debora <debora.roldanlara@gmail.com>
2023-02-13 17:17:23 -0300

    header - Promeira versão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
